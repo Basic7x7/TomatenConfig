f4f4c506dabee7c027efa2c68dfc5f9795e479f4
Basic7x7 <basic7x7@gmail.com>
2025-03-04 20:46:48 +0100

    JavaDoc TomatenConfig and ConfigType

--- src/dev/tomaten/config/ConfigType.java
+++ src/dev/tomaten/config/ConfigType.java
@@ -1,7 +1,25 @@
 package dev.tomaten.config;
 
+/**
+ * The type of a configuration file.
+ * This is used to determine which parser should be used when reading a configuration.
+ */
 public enum ConfigType {
+	
+	/**
+	 * Automatically detect the config type based on the file extension of the configuration file.
+	 * This type cannot be used if the input is not read from a file.
+	 */
 	AUTO_DETECT,
+	
+	/**
+	 * Uses the JSON parser to parse the configuration.
+	 * The parser will be able to read non-strict JSON.
+	 */
 	JSON,
+	
+	/**
+	 * Uses the TOML parser to parse the configuration.
+	 */
 	TOML
 }
--- src/dev/tomaten/config/TomatenConfig.java
+++ src/dev/tomaten/config/TomatenConfig.java
@@ -5,14 +5,28 @@ import static de.tomatengames.util.RequirementUtil.requireNotNull;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.function.Supplier;
 
 import de.tomatengames.lib.compiler.CompilerException;
 
+/**
+ * The main class of the TomatenConfig project.
+ * <p>
+ * This class provides methods to load configurations from files and other sources.
+ * 
+ * @version 2025-03-04 last modified
+ * @version 2025-02-15 created
+ * @since 1.0
+ */
 public class TomatenConfig {
 	
+	/**
+	 * Returns the currently running version of TomatenConfig.
+	 * @return The version string. Not null.
+	 */
 	public static String getVersion() {
 		return "1.0-dev";
 	}
@@ -39,22 +53,56 @@ public class TomatenConfig {
 	}
 	
 	
+	/**
+	 * Reads a configuration from the specified {@link Path}.
+	 * The file extension of the Path determines the parser to use.
+	 * The file is expected to be encoded in UTF-8.
+	 * @param <C> The type of the configuration to read.
+	 * @param configFactory A factory that creates an uninitialized instance of the configuration to read. Not null. For example, {@code Config::new}.
+	 * @param path The {@link Path} to the configuration file that should be read. Not null.
+	 * @return The configuration that was read. Not null.
+	 * @throws ConfigError If the configuration could not be read or parsed.
+	 * This may also wrap an {@link IOException}.
+	 */
 	public static <C extends AbstractConfig<C>> C load(Supplier<C> configFactory, Path path) throws ConfigError {
 		return load(configFactory, path, ConfigType.AUTO_DETECT);
 	}
 	
+	/**
+	 * Reads a configuration from the specified {@link Path}.
+	 * The {@link ConfigType} determines which parser should be used.
+	 * The file is expected to be encoded in UTF-8.
+	 * @param <C> The type of the configuration to read.
+	 * @param configFactory A factory that creates an uninitialized instance of the configuration to read. Not null. For example, {@code Config::new}.
+	 * @param path The {@link Path} to the configuration file that should be read. Not null.
+	 * @param type The {@link ConfigType} that determines which parser should be used. Not null.
+	 * @return The configuration that was read. Not null.
+	 * @throws ConfigError If the configuration could not be read or parsed.
+	 * This may also wrap an {@link IOException}.
+	 */
 	public static <C extends AbstractConfig<C>> C load(Supplier<C> configFactory, Path path, ConfigType type) throws ConfigError {
 		type = detectConfigType(path, type);
 		if (type == ConfigType.AUTO_DETECT) {
 			throw new ConfigError("Unsupported config file extension: " + path);
 		}
-		try (BufferedReader reader = Files.newBufferedReader(path)) {
+		try (BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
 			return load(configFactory, reader, type);
 		} catch (IOException e) {
 			throw new ConfigError("Failed to read the config file", e);
 		}
 	}
 	
+	/**
+	 * Reads a configuration from the specified {@link Reader}.
+	 * The {@link ConfigType} determines which parser should be used.
+	 * @param <C> The type of the configuration to read.
+	 * @param configFactory A factory that creates an uninitialized instance of the configuration to read. Not null. For example, {@code Config::new}.
+	 * @param reader The {@link Reader} to read the configuration from. Not null.
+	 * @param type The {@link ConfigType} that determines which parser should be used. Not null.
+	 * @return The configuration that was read. Not null.
+	 * @throws ConfigError If the configuration could not be read or parsed.
+	 * This may also wrap an {@link IOException}.
+	 */
 	public static <C extends AbstractConfig<C>> C load(Supplier<C> configFactory, Reader reader, ConfigType type) throws ConfigError {
 		requireNotNull(type, "The config type ...");
 		
